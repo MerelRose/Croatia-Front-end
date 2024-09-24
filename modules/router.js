1c78d7e0663cc933bae07c87ef10087893fbeb48
brianp17 <bpostzakelijk@gmail.com>
2024-09-24 11:25:48 +0200

    bug fixes | router done

--- modules/router.js
+++ modules/router.js
@@ -1,6 +1,6 @@
 export class Router {
 
-    constructor(){
+    constructor() {
         this.routes = {
             '/': {
                 page: 'CFE-home.html'
@@ -12,19 +12,36 @@ export class Router {
                 page: 'LeonTest.html'
             }
         };
+
+        // Bind the 'handleHashChange' method to the current class instance
+        this.handleHashChange = this.handleHashChange.bind(this);
+
+        // Listen to the hash change event
+        window.addEventListener('hashchange', this.handleHashChange);
+
+        // Handle the initial load
+        this.handleHashChange();
+    }
+
+    handleHashChange() {
+        // Get the current route from the hash, defaulting to '/' if empty
+        const route = window.location.hash.slice(1) || '/';
+
+        console.log(route);
+
+        // Navigate to the route
+        this.navigate(route);
     }
 
     async navigate(route) {
         console.log(route);
-        if (!this.routes[route]) return document.getElementById('app').innerHTML = '<h3>404 not found</h3>';
+        if (!this.routes[route]) {
+            return document.getElementById('app').innerHTML = '<h3>404 Not Found</h3>';
+        }
 
         const res = await fetch('../pages/' + this.routes[route].page);
 
-        window.history.pushState({}, '', route);
+        // Update the content of the app div with the page content
         document.getElementById('app').innerHTML = await res.text();
-    }
-
-    handlePopState() {
-        this.navigate(window.location.pathname)
     }
 }
