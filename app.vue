5a2519a4f0f5306bb2c3b5c6aca40b72a9a1bfba
brianp17 <bpostzakelijk@gmail.com>
2024-09-25 10:46:46 +0200

    nuxt

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup>
+  useHead({
+    script: [
+      {
+        src: 'https://kit.fontawesome.com/dd14f00971.js',
+        crossorigin: 'anonymous'
+      }
+    ]
+  })
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="_layout">
+    <AppHeader />
+    <main>
+      <slot />
+    </main>
+  </div>
+  <AppFooter />
+</template>
+
+<style scoped>
+
+</style>
