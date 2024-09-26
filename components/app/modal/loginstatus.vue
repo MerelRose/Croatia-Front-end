934af9a47c605aec2ef202a0f93b5940bf45221e
JordytheCodemaster <124360795+JordytheCodemaster@users.noreply.github.com>
2024-09-26 11:52:23 +0200

    branch switch

---
+++ components/app/modal/loginstatus.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button v-if="isLoggedIn" @click="logout">Logout</button>
+    <button v-else @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoggedIn: false
+    }
+  },
+  mounted() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.isLoggedIn = true;
+    }
+  },
+  methods: {
+    login() {
+      // Login logic here
+    },
+    logout() {
+      // Logout logic here
+      localStorage.removeItem('token');
+      this.isLoggedIn = false;
+    }
+  }
+}
+</script>
